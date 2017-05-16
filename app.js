c52a20df3ff6f3d28f29f10112d31040d807c9ef
chriscaraway <chrisccaraway@gmail.com>
2017-05-16 17:18:46 -0400

    update

--- app.js
+++ app.js
@@ -5,20 +5,56 @@ const app = express();
 app.set("view engine", "ejs");
 // app.use(static("assets"));
 
+app.get("/ejs", function(req, res) {
+	res.render("", {
+		ifEjs: true,
+		list: ["list", "of", "things"],
+	});
+});
 
-// app.get("/ejs", function(req, res) {
-// 	res.render("", {
-// 		ifEjs: true,
-// 		list: ["list", "of", "things"],
-// 	});
-// });
+function renderTemplate(res, page, title, pageArgs) {
+	return res.render("template", {
+		page: page,
+		title: title,
+		pageArgs: pageArgs,
+	});
+}
+
+app.get(("/"), function(req, res) {
+	renderTemplate(res, "home", "Homepage", {
+		links: [{
+			text: "EJS Example",
+			href: "/ejs",
+		}, {
+			text: "About",
+			href: "/about",
+	}],
+});
+});
+
+app.get("/about", function(req, res) {
+	renderTemplate(res, "about", "About", {
+		picture: "images/chris.jpeg",
+		linkedin: "www.linkedin.com/in/christophercaraway",
+});
+});
+
 
 app.get("/chriscaraway", function(req, res) {
 	console.log("Chris Caraway Hompepage");
 	res.render("chriscaraway");
 });
 
+//
+// app.get("/about", function(req, res) {
+// 	renderTemplate(res, "about", "About", {
+// 	picture: " ",
+// 	linkedin: "ChrisCaraway",
+// });
+
+
+
 
 app.listen(3000, function() {
-	console.log("Your server is available at localhost:3000!");
+	console.log("Your server is available at http://localhost:3000!");
 });
