a25d3ceb9077b3d76bb6d1182bc06c2f4d5340d9
chriscaraway <chrisccaraway@gmail.com>
2017-05-15 21:00:42 -0400

    update

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const app = express();
 
+
 app.set("view engine", "ejs");
+// app.use(static("assets"));
 
-app.get("Title", function(req, res) {
-	console.log("Serving up file");
-	res.render("Title");
+
+// app.get("/ejs", function(req, res) {
+// 	res.render("", {
+// 		ifEjs: true,
+// 		list: ["list", "of", "things"],
+// 	});
+// });
+
+app.get("/chriscaraway", function(req, res) {
+	console.log("Chris Caraway Hompepage");
+	res.render("chriscaraway");
 });
-
-app.get("AboutMe", function(req, res) {
-	res.render("About Me");
-});
-
-app.get("Photos", function(req, res) {
-	res.render("Photos");
-});
-
-app.get("WorkSamples", function(req, res) {
-	res.render("Work Samples");
-});
-
-
-
-
 
 
 app.listen(3000, function() {
-	console.log("Your server is available at http://localhost:3000!");
+	console.log("Your server is available at localhost:3000!");
 });
